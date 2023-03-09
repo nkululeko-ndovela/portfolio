3b34e992bb0ace7b9f77dd168ddb069fbbcbb8ec
nkululeko-ndovela <ndovelankululekoree1@gmail.com>
2023-03-09 10:06:43 +0200

    added new features, test case, Enable CL Intergration

--- portfolio_app/tests.py
+++ portfolio_app/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@example.com', password='testpassword')
+        self.user_profile = UserProfile.objects.create(
+            user=self.user, home_address='123 Test Street', phone_number='1234567890')
+    
+    def test_profile_view(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 302)
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'User Profile')
+        self.assertContains(response, '123 Street')
+        self.assertContains(response, '88546834765')
+    
+    def test_edit_profile_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post('/edit_profile/', {'home_address': '456  Street', 'phone_number': '345353453'})
+        self.assertEqual(response.status_code, 200)
+        user_profile = UserProfile.objects.get(user=self.user)
+        self.assertEqual(user_profile.home_address, '456Street')
+        self.assertEqual(user_profile.phone_number, '9876543210')
+
--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2qgsz)!f5j3vgpv^7*n=@19$ps5i4mb(1x#_!lg)-kmu4y^i*i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
